cc1decce47c8d9b07317d8b66d28c741fb49d166
ANDREW RAMELL <andrew.ramell@gmail.com>
2017-01-18 01:06:07 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
         levitation_force = 6
+        counter = 6
+        until counter == 10
+          puts "Wingardium Leviosa"
+          counter +=1
+        end
         #your code here
-    
-end
 
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while
 	levitation_force = 6
-	
+	counter = 6
+  while counter < 10
+    puts "Wingardium Leviosa"
+		counter +=1
+	end
+
 	#your code here
 end
-
-
