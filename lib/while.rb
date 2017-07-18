ac8650fbb1dda4fdfa2e93ae599c707d5133e23a
Briana Gross <brianagross@me.com>
2017-07-18 22:20:02 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,9 @@
 def using_while
 	levitation_force = 6
-	
-	#your code here
+	while levitation_force < 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
 end
 
-
+#I'm not totally sure, but I think that this is all put inside a method to make it easier to test. You can also just write the while loop by itself in IRB.
