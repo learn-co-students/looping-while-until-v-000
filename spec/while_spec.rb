cbbf4c2f273c6e3fd8e2d59cbb09c66b481d2faa
alexis101 <kondilisalexis@gmail.com>
2017-11-22 15:38:03 +0000

    Done.

--- spec/while_spec.rb
+++ spec/while_spec.rb
@@ -1,9 +1,19 @@
 require_relative "spec_helper"
 require_relative "../lib/while.rb"
 
-describe "#using_while" do 
-	it "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10" do 
+describe "#using_while" do
+	it "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10" do
 	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
 	expect{ using_while }.to output(looping_string).to_stdout
 	end
+end
+
+
+def using_while ()
+  i = 6
+	while i < 10
+	puts "Wingardium Leviosa"
+	i += 1
+
+end
 end
