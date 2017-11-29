7333432fb9e4ca10b96cd8211252151f388824a8
Aubrey Neimeier <neimeier.1@osu.edu>
2017-11-29 17:46:31 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while
 	levitation_force = 6
-	while levitation_force <10
+	while levitation_force < 10
 		levitation_force += 1
 		puts "Wingardium Leviosa"
 	end
 end
+
+
+#it does not matter if increment is before or after the code execution. 
