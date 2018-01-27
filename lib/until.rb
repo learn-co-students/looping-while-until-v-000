55a8a49f859594b2ceabf80ab44725ffc3f8d985
Brandon Brasson <rocksong622@gmail.com>
2018-01-27 20:07:50 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,5 @@
 def using_until
-        levitation_force = 6
+        levitation_force = 1
         #your code here
-    
-end
 
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,5 @@
 def using_while
-	levitation_force = 6
-	
+	levitation_force = 10 === 10
+
 	#your code here
 end
-
-
