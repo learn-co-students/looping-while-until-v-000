edebbc714e7508726b8025d3a88ae2448e7accac
Jordan Krueger <jkrueger702@gmail.com>
2018-05-17 05:24:00 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
-        levitation_force = 6
-        #your code here
-    
+	levitation_force = 6
+	until levitation_force == 7
+  num_of_hotdogs_eaten += 1
+  puts "Wingardium Leviosa"
 end
-
+  end
