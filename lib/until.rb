3a3c76d25e18dc0e2534d2139bf387bd0a00454e
Alex Roman <elpitbull6989@gmail.com>
2018-06-25 20:53:38 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -2,9 +2,10 @@ def using_until
         levitation_force = 6
         #your code here
     until levitation_force == 10
+      #check wether levitation_force is equal to ten
       puts "Wingardium Leviosa"
 
       levitation_force += 1
-
+#increase levitation_force by one with every iteration
     end
 end
