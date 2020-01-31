d3583792c80cd22ab6be5fad05d19495494770c8
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-31 15:08:16 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
 def using_while
   levitation_force = 6
-	
-  #your code here
+	while levitation_force < 10
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+end
 end
 
-
+using_while  #this is calling the method so that i can run the code in the terminal
+             # you also have to remember to map your file and folder in the terminal
+           #when you run the code so that ruby knows how to find it
